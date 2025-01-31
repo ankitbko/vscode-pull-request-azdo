1c11f50b9dd08cd90309865faa72f5d52391190b
Ankit Sinha <anksinha@microsoft.com>
2025-01-31 20:40:42 +0530

    feat: enhance CredentialStore to infer organization configuration from Git

--- src/azdo/credentials.ts
+++ src/azdo/credentials.ts
@@ -2,6 +2,7 @@ import * as azdev from 'azure-devops-node-api';
 import { IRequestHandler } from 'azure-devops-node-api/interfaces/common/VsoBaseInterfaces';
 import { Identity } from 'azure-devops-node-api/interfaces/IdentitiesInterfaces';
 import * as vscode from 'vscode';
+import { IGit } from '../api/api';
 import Logger from '../common/logger';
 import { ITelemetry } from '../common/telemetry';
 import { SETTINGS_NAMESPACE } from '../constants';
@@ -12,6 +13,10 @@ const PATTOKEN_SETTINGS = 'patToken';
 const TRY_AGAIN = vscode.l10n.t('Try again?');
 const CANCEL = vscode.l10n.t('Cancel');
 const ERROR = vscode.l10n.t('Error signing in to Azure DevOps');
+
+class AzdoOrgConfig {
+	constructor(public orgUrl: string, public projectName: string) {}
+}
 
 export class Azdo {
 	private _authHandler: IRequestHandler;
@@ -35,14 +40,14 @@ export class Azdo {
 export class CredentialStore implements vscode.Disposable {
 	static ID = 'AzdoRepository';
 	private _azdoAPI: Azdo | undefined;
-	private _orgUrl: string | undefined;
+	private orgConfig: AzdoOrgConfig | undefined;
 	private _disposables: vscode.Disposable[];
 	private _onDidInitialize: vscode.EventEmitter<void> = new vscode.EventEmitter();
 	public readonly onDidInitialize: vscode.Event<void> = this._onDidInitialize.event;
 	private _sessionId: string | undefined;
 	private _sessionOptions: vscode.AuthenticationGetSessionOptions = { createIfNone: true };
 
-	constructor(private readonly _telemetry: ITelemetry, private readonly _secretStore: vscode.SecretStorage) {
+	constructor(private readonly _telemetry: ITelemetry, private readonly _secretStore: vscode.SecretStorage, private readonly _gitAPI: IGit) {
 		this._disposables = [];
 		this._disposables.push(
 			vscode.authentication.onDidChangeSessions(async () => {
@@ -83,27 +88,70 @@ export class CredentialStore implements vscode.Disposable {
 		this._azdoAPI = undefined;
 	}
 
+	public inferOrgConfigFromGit(git: IGit): [string, string] {
+		// TODO: Need better way of handling multiple repositories. CredentialStore should be initialized within each FolderRepositoryManager and scoped to particular AzDORepository.
+		if (git.repositories.length !== 1) {
+			Logger.appendLine(`Unable to infer org config from git. Repository Length: ${git.repositories.length}`, CredentialStore.ID);
+			return [undefined, undefined];
+		}
+		if (git.repositories[0].state.remotes.length !== 1) {
+			Logger.appendLine(`Unable to infer org config from git. Remote Length: ${git.repositories[0].state.remotes.length}. Remotes: ${git.repositories[0].state.remotes.map(r => r.name).join(',')}`, CredentialStore.ID);
+			return [undefined, undefined];
+		}
+
+		const fetchUrl = git.repositories[0].state.remotes[0].fetchUrl;
+		// Assumption for fetchUrl: https://<org>@dev.azure.com/<org>/<project>/_git/<repo>
+
+		Logger.appendLine('fetchUrl: ' + fetchUrl, CredentialStore.ID);
+		const orgUrlMatch = fetchUrl.match(/https:\/\/(.+?)@dev\.azure\.com\/(.+?)\//);
+		Logger.debug(`orgUrlMatch: ${orgUrlMatch}`, CredentialStore.ID);
+
+		const orgUrl = orgUrlMatch && orgUrlMatch.length > 2 ? `https://dev.azure.com/${orgUrlMatch[2]}` : undefined; // should be parsed to https://dev.azure.com/<org>
+		const projectNameMatch = fetchUrl.match(/\/([^\/]+)\/_git\//);
+		Logger.debug(`projectNameMatch: ${projectNameMatch}`, CredentialStore.ID);
+
+		const projectName = projectNameMatch && projectNameMatch.length > 1 ? projectNameMatch[1] : undefined; // should be parsed to <project>
+		Logger.appendLine(`Inferred orgUrl: ${orgUrl}, projectName: ${projectName}`, CredentialStore.ID);
+
+		return [orgUrl, projectName];
+	}
+
+	public getOrgConfig(): AzdoOrgConfig | undefined {
+		let projectName = vscode.workspace.getConfiguration(SETTINGS_NAMESPACE).get<string | undefined>(PROJECT_SETTINGS);
+		let orgUrl = vscode.workspace.getConfiguration(SETTINGS_NAMESPACE).get<string | undefined>(ORGURL_SETTINGS);
+
+		if (!projectName || !orgUrl) {
+			[orgUrl, projectName] = this.inferOrgConfigFromGit(this._gitAPI);
+		}
+
+		if (!projectName) {
+			Logger.appendLine('Project name is not provided', CredentialStore.ID);
+			this._telemetry.sendTelemetryEvent('auth.failed');
+			return undefined;
+		}
+
+		if (!orgUrl) {
+			Logger.appendLine('orgUrl is not provided', CredentialStore.ID);
+			this._telemetry.sendTelemetryEvent('auth.failed');
+			return undefined;
+		}
+
+		Logger.appendLine(`orgUrl is ${orgUrl}. Project name is ${projectName}`, CredentialStore.ID);
+		return new AzdoOrgConfig(orgUrl, projectName);
+	}
+
 	public async login(): Promise<Azdo | undefined> {
 		/* __GDPR__
 			"auth.start" : {}
 		*/
 		this._telemetry.sendTelemetryEvent('auth.start');
 
-		const projectName = vscode.workspace.getConfiguration(SETTINGS_NAMESPACE).get<string | undefined>(PROJECT_SETTINGS);
-		if (!projectName) {
-			Logger.appendLine('Project name is not provided', CredentialStore.ID);
+		const orgConfig = this.getOrgConfig();
+		if (!orgConfig) {
+			Logger.appendLine('Unable to get org config', CredentialStore.ID);
 			this._telemetry.sendTelemetryEvent('auth.failed');
 			return undefined;
 		}
-		Logger.appendLine(`projectName is ${projectName}`, CredentialStore.ID);
-
-		this._orgUrl = vscode.workspace.getConfiguration(SETTINGS_NAMESPACE).get<string | undefined>(ORGURL_SETTINGS);
-		if (!this._orgUrl) {
-			Logger.appendLine('orgUrl is not provided', CredentialStore.ID);
-			this._telemetry.sendTelemetryEvent('auth.failed');
-			return undefined;
-		}
-		Logger.appendLine(`orgUrl is ${this._orgUrl}`, CredentialStore.ID);
 
 		let retry: boolean = true;
 
@@ -133,7 +181,7 @@ export class CredentialStore implements vscode.Disposable {
 					isPatTokenAuth = false;
 				}
 
-				const azdo = new Azdo(this._orgUrl, projectName, token, isPatTokenAuth);
+				const azdo = new Azdo(orgConfig.orgUrl, orgConfig.projectName, token, isPatTokenAuth);
 				azdo.authenticatedUser = (await azdo.connection.connect()).authenticatedUser;
 
 				Logger.debug(`Auth> Successful: Logged userid: ${azdo?.authenticatedUser?.id}`, CredentialStore.ID);
